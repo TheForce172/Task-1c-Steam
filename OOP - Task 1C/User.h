ddd1351d35f7fc116b052e1548b991d418e7d713
Cranston <theforce172@gmail.com>
2019-03-19 21:06:52 +0000

    add credits attribute

--- OOP - Task 1C/User.h
+++ OOP - Task 1C/User.h
@@ -5,11 +5,12 @@
 class User
 {
 	public:
-		User(const std::string&, const std::string&, const std::string&);
+		User(const std::string&, const std::string&, const std::string&, const int&);
 		virtual ~User();
 		const std::string& GetUsername() const;
 	private:
 		std::string username;
 		std::string password;
 		std::string created;  // TODO: replace with custom Date class, currently YYYY-MM-DD
+		int credits;
 };
