05a5c644a7dc398ddd2e646768fefde065b9f464
Cranston <theforce172@gmail.com>
2019-04-05 21:06:20 +0100

    Update ProfileMenu.cpp

--- OOP - Task 1C/ProfileMenu.cpp
+++ OOP - Task 1C/ProfileMenu.cpp
@@ -4,8 +4,10 @@
 
 ProfileMenu::ProfileMenu(const std::string& title, Application * app, User* account) : Menu(title, app), account(account)
 {
-	for (int i = 0; i < account->getLibary().length(); i++) {
-		games.addAtEnd(account->getLibary()[i]->getGame());
+	if (!(account->getLibary().isEmpty())) {
+		for (int i = 0; i < account->getLibary().length(); i++) {
+			games.addAtEnd(account->getLibary()[i]->getGame());
+		}
 	}
 	Paint(); // required in constructor
 
