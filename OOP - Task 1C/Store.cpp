0ae4d98ebbbe5f5ecf366985ef777a97b6aab4e0
Cranston <theforce172@gmail.com>
2019-04-07 16:14:29 +0100

    remove dead code

--- OOP - Task 1C/Store.cpp
+++ OOP - Task 1C/Store.cpp
@@ -18,26 +18,6 @@ void Store::addGame(Game* g) {
 	games.addAtEnd(g);
 }
 
-/*void Store::SearchGames() const {
-	StoreMenu.showSearchMenu();
-	int option = StoreMenu.readInSearchOption();
-	switch (option)
-	{
-	case 0:
-		showSearchForName();
-		break;
-	case 1:
-		showSearchForageRating();
-		break;
-	case 2:
-		showSearchForCost();
-	case 3:
-		return;
-	default:
-		break;
-	}
-}*/
-
 void Store::showSearchForName() const {
 
 
